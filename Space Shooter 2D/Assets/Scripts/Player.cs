3173da14a088244c129cd932a96c26ff14bf97d8
forrest10351 <forrest10351@gmail.com>
2020-01-01 10:11:03 -0600

    finished episode 88
    finished episode and challenge for ep 88

--- Space Shooter 2D/Assets/Scripts/Player.cs
+++ Space Shooter 2D/Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float playerSpeed = 10f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +18,15 @@ public class Player : MonoBehaviour
         move();
     }
 
-    private move()
+    private void move()
     {
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
+        var newYPos = transform.position.y + deltaY;
 
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime*playerSpeed;
+        var newXPos = transform.position.x + deltaX;
+
+
+        transform.position = new Vector2(newXPos, newYPos);
     }
 }
