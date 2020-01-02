984e1d0fea3e8199be2cf73ef34eb70669058994
forrest10351 <forrest10351@gmail.com>
2020-01-02 07:34:47 -0600

    finished episode 92
    project currently has an example coroutine in it called test, which is called in the start method, this will be deleted.
    additionally I renamed the bulk assets folder so it would be at the bottom

--- Space Shooter 2D/Assets/Scripts/Player.cs
+++ Space Shooter 2D/Assets/Scripts/Player.cs
@@ -6,36 +6,48 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 10f;
-
+    [SerializeField] float padding = 1f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float laserSpeed = 20f;
 
     float xMin;
     float xMax;
     float yMin;
     float yMax;
-    [SerializeField] float padding=1f;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(test());
         setPlayerBounds();
     }
 
  
-    private void setPlayerBounds()
-    {
-        Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint (new Vector3(0f, 0f, 0f)).x+padding;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1f, 0f, 0f)).x-padding;
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).y+padding;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0f, 1f, 0f)).y-padding;
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
         move();
+        Fire();
+        
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1")==true)
+        {
+            //my instinct would have been to have a laser script in order to manipulate the laser
+            // would be interested in the pros and cons vs this way of controlling the laser through this script
+            GameObject laser = 
+                Instantiate(laserPrefab, transform.position, Quaternion.identity)
+                as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+
+
+        }
     }
 
     private void move()
@@ -46,8 +58,24 @@ public class Player : MonoBehaviour
         var newXPos = Mathf.Clamp(transform.position.x + deltaX,xMin,xMax);
         var newYPos = Mathf.Clamp(transform.position.y + deltaY,yMin,yMax);
 
-
-
         transform.position = new Vector2(newXPos, newYPos);
     }
+    private void setPlayerBounds()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1f, 0f, 0f)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0f, 1f, 0f)).y - padding;
+    }
+
+    
+    
+    IEnumerator test() 
+    {
+        print("test1");
+        yield return new WaitForSeconds(3f);
+        print("test dos");
+    }
+    
 }
