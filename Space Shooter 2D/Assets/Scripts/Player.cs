ccea861219a99a2e6246a6147c19d05c52402122
forrest10351 <forrest10351@gmail.com>
2020-01-01 19:42:52 -0600

    finished episode 90

--- Space Shooter 2D/Assets/Scripts/Player.cs
+++ Space Shooter 2D/Assets/Scripts/Player.cs
@@ -1,15 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 10f;
-    
+
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    [SerializeField] float padding=1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
         
+        setPlayerBounds();
+    }
+
+ 
+    private void setPlayerBounds()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint (new Vector3(0f, 0f, 0f)).x+padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1f, 0f, 0f)).x-padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).y+padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0f, 1f, 0f)).y-padding;
     }
 
     // Update is called once per frame
@@ -21,10 +41,11 @@ public class Player : MonoBehaviour
     private void move()
     {
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
-        var newYPos = transform.position.y + deltaY;
-
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime*playerSpeed;
-        var newXPos = transform.position.x + deltaX;
+        
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX,xMin,xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY,yMin,yMax);
+
 
 
         transform.position = new Vector2(newXPos, newYPos);
