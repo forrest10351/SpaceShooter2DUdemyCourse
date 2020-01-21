e9291b3e2fcda8317c7d8b62ce1f1f4fb50cab31
forrest10351 <forrest10351@gmail.com>
2020-01-21 09:06:39 -0600

    finished episode 95+96
    1-20-20

---
+++ Space Shooter 2D/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Transform> EnemyMovementPath;
+    [SerializeField] float moveSpeed = 2f;
+    
+    int wayPointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = EnemyMovementPath[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (wayPointIndex <= EnemyMovementPath.Count - 1)
+        {
+            var targetPosition = EnemyMovementPath[wayPointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            if (transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
