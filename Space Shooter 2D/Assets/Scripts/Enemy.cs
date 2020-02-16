ed4d97b334ae4672530c4d754a97d0a9a40b6526
forrest10351 <forrest10351@gmail.com>
2020-02-16 09:17:48 -0600

    changes done before 2/16
    not sure which episode this leaves off on, but i had not commited these changes to the master.

--- Space Shooter 2D/Assets/Scripts/Enemy.cs
+++ Space Shooter 2D/Assets/Scripts/Enemy.cs
@@ -1,20 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = .2f;
+    [SerializeField] float maxTimeBetweenShots = .2f;
+    [SerializeField] GameObject laser;
+    [SerializeField] float laserSpeed=-5f;
+
     // Start is called before the first frame update
     void Start()
     {
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+    private void Fire()
+    {
+        GameObject laserfired = Instantiate(laser, transform.position, Quaternion.identity);
+        laserfired.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
